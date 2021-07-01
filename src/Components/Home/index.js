d9b3563e285459b91ffb8b20a1992c2499ea6ce5
sid6145 <sidtdesh@gmail.com>
2021-07-01 13:47:06 +0530

    modified the image to make it responsive and fit properly

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,19 +1,17 @@
-import React from 'react'
-import './style.css'
+import React from "react";
+import "./style.css";
+
+function Home() {
+  return (
+    <div className="home-container">
+      <h2>Welcome to my portfolio!</h2>
+      <p>I am Siddhant Deshmukh, a web developer</p>
 
 
-function Home (){
-    return(
-      
-            <div className="home-container">
-            <img src="images/home-img.svg" />
-            <h2>Welcome to my portfolio!</h2>
-            <p>I am Siddhant Deshmukh, a web developer</p>
-           
-            </div>
-         
-       
-    )
+        <img src="images/home-img.svg" />
+ 
+    </div>
+  );
 }
 
-export default Home
+export default Home;
