c414bada46335dd2778348d897e6019952254807
sid6145 <sidtdesh@gmail.com>
2021-07-04 14:50:56 +0530

    linked home page buttons to their respective pages

--- src/Components/Home/index.js
+++ src/Components/Home/index.js
@@ -1,19 +1,33 @@
 import React from "react";
-import {Button} from 'react-bootstrap'
+import { Link } from "react-router-dom";
 import "./style.css";
 
 function Home() {
   return (
     <div className="home-container">
       <h2>Welcome to my portfolio!</h2>
-      <p>I am Siddhant Deshmukh, a <span style={{color:"#1F3C88", fontFamily:"Unica One, cursive", fontSize:"30px",
-        fontWeight:"600"
-    
-    }}>Web developer</span></p>
-        <div className="btn-container">
-        <button className="btn btn-about">About me</button>
-        <button className="btn btn-project">My projects</button>
-        </div>
+      <p>
+        I am Siddhant Deshmukh, a{" "}
+        <span
+          style={{
+            color: "#1F3C88",
+            fontFamily: "Unica One, cursive",
+            fontSize: "30px",
+            fontWeight: "600",
+          }}
+        >
+          Web developer
+        </span>
+      </p>
+      <div className="btn-container">
+        <Link to="/about">
+          <button className="btn btn-about">About me</button>
+        </Link>
+
+        <Link to="/projects">
+          <button className="btn btn-project">My projects</button>
+        </Link>
+      </div>
     </div>
   );
 }
