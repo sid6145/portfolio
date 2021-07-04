d6e6ae058c50153982b8d8025da610d94524f603
sid6145 <sidtdesh@gmail.com>
2021-07-04 14:48:38 +0530

    completed skills page

--- src/Components/Skills/index.js
+++ src/Components/Skills/index.js
@@ -1,9 +1,36 @@
 import React from 'react'
+import './style.css'
 
 function Skills() {
     return (
-        <div>
-            <h1>skills page</h1>
+        <div className="skills-container">
+            <h1>Skills</h1>
+            <div className="skill-list">
+            <div className="skill">
+                <img src="images/node.png" />
+                <h3>Node Js</h3>
+            </div>
+            <div className="skill">
+                <img src="images/js.png" />
+                <h3>JavaScript</h3>
+            </div>
+            <div className="skill">
+                <img src="images/express.png" />
+                <h3>Express Js</h3>
+            </div>
+            <div className="skill">
+                <img src="images/html.png" />
+                <h3>HTML5</h3>
+            </div>
+            <div className="skill">
+                <img src="images/css.png" />
+                <h3>CSS3</h3>
+            </div>
+            <div className="skill">
+                <img src="images/react.png" />
+                <h3>React Js</h3>
+            </div>
+            </div>
         </div>
     )
 }
